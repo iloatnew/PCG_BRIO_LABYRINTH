2602ead55627741de9a4183781aafbff28c255e9
oatnewil <wentao.li@tu-dortmund.de>
2020-04-02 23:25:04 +0200

    first commit

---
+++ Assets/Scripts/DFSMazeMutator.cs
@@ -0,0 +1,232 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DFSMazeMutator : MonoBehaviour
+{
+
+	private enum Dir { north = 1, south = 2, east = 3, west = 4, none = 0 };
+	private MazeCell[,] mazeCells;
+	int mazeRows;
+	int mazeColumns;
+	MazeHelp mazeHelp;
+
+	public DFSMazeMutator(MazeHelp mazeHelp)
+	{
+		this.mazeCells = mazeHelp.mazeCells;
+		this.mazeRows = mazeHelp.mazeRows;
+		this.mazeColumns = mazeHelp.mazeColumns;
+		this.mazeHelp = mazeHelp;
+	}
+
+	/// <summary>
+	/// DFS to calculate the critical path
+	/// </summary>
+	public void DFS()
+	{
+		int currentColumn = 0;
+		int currentRow = 0;
+		int nextColumn = 0;
+		int nextRow = 0;
+		Dir fromDirection = Dir.none;
+		MazeCell currentCell = mazeCells[currentRow, currentColumn];
+		MazeCell lastCell = currentCell;
+		bool finish = false;
+		int pace = 0;
+
+		var possibleNextPosition = 0;
+
+		while (!finish && pace++ < mazeRows * mazeColumns * 2)
+		{
+			//Debug.Log("Now At " + "[" + currentRow + "," + currentColumn + "]" + " SavePoint is " + mazeCells[currentRow, currentColumn].savePoint + " Pace: " + pace);
+
+			// fin?
+			if (currentColumn == mazeColumns - 1 && currentRow == mazeRows - 1)
+			{
+				finish = true;
+				Debug.Log("DFSsolved");
+				return;
+			}
+
+			// note current cell
+			currentCell = mazeCells[currentRow, currentColumn];
+
+			// mark it as critical path (might be delete later)
+			currentCell.inCriticalPath = true;
+
+			// mark this as DFS visited
+			currentCell.dfsVisited = true;
+
+			// if last cell is the save point of this cell, restart path tracking
+			if (lastCell == mazeCells[(int)currentCell.savePoint.x, (int)currentCell.savePoint.y])
+			{
+				currentCell.pathTilLastSaveP.Clear();
+				currentCell.pathTilLastSaveP.Add(lastCell);
+				currentCell.pathTilLastSaveP.Add(currentCell);
+			}
+			// if just jumped back, do nothing. Only extend path by a normal walk
+			else if (possibleNextPosition != 0)
+			{
+				// take path from last cell, add this cell in path
+				currentCell.pathTilLastSaveP = lastCell.pathTilLastSaveP;
+				currentCell.pathTilLastSaveP.Add(currentCell);
+			}
+
+			// calculate next position
+			possibleNextPosition = PossibleNextPosition(currentRow, currentColumn, fromDirection);
+
+			// move to Direction
+			switch (possibleNextPosition)
+			{
+				//north
+				case 1:
+					nextRow = currentRow - 1;
+					fromDirection = Dir.south;
+					break;
+				//south
+				case 2:
+					nextRow = currentRow + 1;
+					fromDirection = Dir.north;
+					break;
+				//east
+				case 3:
+					nextColumn = currentColumn + 1;
+					fromDirection = Dir.west;
+					break;
+				//west
+				case 4:
+					nextColumn = currentColumn - 1;
+					fromDirection = Dir.east;
+					break;
+				//cant move, preform jump
+				case 0:
+
+					//go back to save point
+					nextRow = (int)currentCell.savePoint.x;
+					nextColumn = (int)currentCell.savePoint.y;
+					fromDirection = Dir.none;
+
+					//mark all cells in path as NONE critical
+					foreach (MazeCell mc in currentCell.pathTilLastSaveP)
+					{
+						mc.inCriticalPath = false;
+					}
+
+					break;
+				default:
+					break;
+			}
+
+			// if gonna go back, then don't set new savePoint
+			if (possibleNextPosition != 0)
+			{
+				// is corss
+				if (currentCell.isCross)
+				{
+					// save this for next points
+					mazeCells[nextRow, nextColumn].savePoint = new Vector2(currentRow, currentColumn);
+
+				}
+				// not cross
+				else
+				{
+					// if this is NOT cross, use the last save point
+					mazeCells[nextRow, nextColumn].savePoint = currentCell.savePoint;
+
+				}
+			}
+
+			// store the last cell
+			lastCell = currentCell;
+
+			// Moved
+			currentRow = nextRow;
+			currentColumn = nextColumn;
+
+		}
+
+	}
+
+	/// <summary>
+	/// Vector(x,y), x: possible direction, y: total number of possible directions
+	/// </summary>
+	private int PossibleNextPosition(int row, int column, Dir fromDirection)
+	{
+
+		var possibleNextPosition = 0;
+		var cntOpen = 0;
+
+		//north has open route & not from north
+		if (mazeHelp.CellIsAvailable(row - 1, column, true) && fromDirection != Dir.north)
+			if (mazeHelp.HasPathToUnvisitedCell(row, column, 1))
+			{
+				possibleNextPosition = 1;
+				cntOpen += 1;
+			}
+
+		//south has open route & not from south
+		if (mazeHelp.CellIsAvailable(row + 1, column, true))
+			if (mazeHelp.HasPathToUnvisitedCell(row, column, 2) && fromDirection != Dir.south)
+			{
+				possibleNextPosition = 2;
+				cntOpen += 1;
+			}
+		//east has open route & same
+		if (mazeHelp.CellIsAvailable(row, column + 1, true))
+			if (mazeHelp.HasPathToUnvisitedCell(row, column, 3) && fromDirection != Dir.east)
+			{
+				possibleNextPosition = 3;
+				cntOpen += 1;
+			}
+		//west has open route
+		if (mazeHelp.CellIsAvailable(row, column - 1, true))
+			if (mazeHelp.HasPathToUnvisitedCell(row, column, 4) && fromDirection != Dir.west)
+			{
+				possibleNextPosition = 4;
+				cntOpen += 1;
+			}
+
+		if (cntOpen > 1)
+		{
+			mazeCells[row, column].isCross = true;
+		}
+		return possibleNextPosition;
+
+	}
+
+	/// <summary>
+	/// build walls when its not in critical path
+	/// </summary>
+	public void CriticalPathOnly()
+	{
+		for (int r = 0; r < mazeRows; r++)
+		{
+			for (int c = 0; c < mazeColumns; c++)
+			{
+				// the northern cell
+				if (mazeHelp.CellIsAvailable(r - 1, c, true))
+					if(mazeCells[r - 1, c].inCriticalPath)
+					{
+						mazeCells[r - 1, c].southOpen = true;
+						mazeCells[r, c].northOpen = true;
+					}
+
+			}
+		}
+		
+
+		//MazeCell goal = mazeCells[mazeRows - 1, mazeColumns - 1];
+		//List<MazeCell> criticalPath = new List<MazeCell>();
+		//criticalPath = goal.pathTilLastSaveP;
+
+		//while (criticalPath != new List<MazeCell>())
+		//{
+		//	criticalPath[0].
+
+
+		//}
+
+
+	}
+
+}
---
+++ Assets/Scripts/HuntAndKillMutated.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+///************************************************************
+/// <summary>
+/// 1. HuntAndKill
+/// 2. DFS
+///</summary>
+///************************************************************
+public class HuntAndKillMutated : MazeAlgorithm
+{
+	private int currentRow = 0;
+	private int currentColumn = 0;
+
+	public bool courseComplete = false;
+
+	private MazeHelp mazeHelp;
+
+	public HuntAndKillMutated(MazeCell[,] mazeCells) : base(mazeCells)
+	{
+		mazeHelp = new MazeHelp(mazeCells, mazeRows, mazeColumns);
+	}
+
+	public override void CreateMaze()
+	{
+		HuntAndKill();
+		
+	}
+
+	private void HuntAndKill()
+	{
+		mazeCells[currentRow, currentColumn].visited = true;
+
+		while (!courseComplete)
+		{
+			Kill(); // Will run until it hits a dead end.
+			Hunt(); // Finds the next unvisited cell with an adjacent visited cell. If it can't find any, it sets courseComplete to true.
+		}
+
+
+		mazeHelp.dFSMazeMutator.DFS();
+		mazeHelp.dFSMazeMutator.CriticalPathOnly();
+
+
+		mazeHelp.DestroyWalls();
+
+	}
+
+
+	private void Kill()
+	{
+		while (mazeHelp.RouteStillAvailable(currentRow, currentColumn))
+		{
+			//int direction = UnityEngine.Random.Range(1, 5);
+			int direction = ProceduralNumberGenerator.GetNextNumber();
+
+			if (direction == 1 && mazeHelp.CellIsAvailable(currentRow - 1, currentColumn))
+			{
+				// North
+				mazeCells[currentRow, currentColumn].northOpen = true;
+				mazeCells[currentRow - 1, currentColumn].southOpen = true;
+				currentRow--;
+			}
+			else if (direction == 2 && mazeHelp.CellIsAvailable(currentRow + 1, currentColumn))
+			{
+				// South
+				mazeCells[currentRow, currentColumn].southOpen = true;
+				mazeCells[currentRow + 1, currentColumn].northOpen = true;
+				currentRow++;
+			}
+			else if (direction == 3 && mazeHelp.CellIsAvailable(currentRow, currentColumn + 1))
+			{
+				// east
+				mazeCells[currentRow, currentColumn].eastOpen = true;
+				mazeCells[currentRow, currentColumn + 1].westOpen = true;
+				currentColumn++;
+			}
+			else if (direction == 4 && mazeHelp.CellIsAvailable(currentRow, currentColumn - 1))
+			{
+				// west
+				mazeCells[currentRow, currentColumn].westOpen = true;
+				mazeCells[currentRow, currentColumn - 1].eastOpen = true;
+				currentColumn--;
+			}
+
+			mazeCells[currentRow, currentColumn].visited = true;
+		}
+	}
+
+	private void Hunt()
+	{
+		courseComplete = true; // Set it to this, and see if we can prove otherwise below!
+
+		for (int r = 0; r < mazeRows; r++)
+		{
+			for (int c = 0; c < mazeColumns; c++)
+			{
+				if (!mazeCells[r, c].visited && mazeHelp.CellHasAnAdjacentVisitedCell(r, c))
+				{
+					courseComplete = false; // Yep, we found something so definitely do another Kill cycle.
+					currentRow = r;
+					currentColumn = c;
+					mazeHelp.DestroyAdjacentWall(currentRow, currentColumn);
+					mazeCells[currentRow, currentColumn].visited = true;
+					return; // Exit the function
+				}
+			}
+		}
+	}
+	
+
+	
+	
+
+
+}
+
